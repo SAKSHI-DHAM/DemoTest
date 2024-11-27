5868400e6ee00a3055b931ebcb98753fa87e10f9
Sakshi <sakshidhamne21@gmail.com>
2024-11-28 00:25:11 +0530

    second commit

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println("HEy this is my second commit");
     }
 }
---
+++ src/main/java/com/example/sakshi.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class sakshi {
+    public static void main(String[] args) {
+        System.out.println("lets beggin the internship task now");
+    }
+}
